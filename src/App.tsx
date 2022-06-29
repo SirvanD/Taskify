5f5d002aaab93560d2f8f4dbbd0a854b97205363
SirvanD <sirvansh@gmail.com>
2022-06-29 11:26:17 +1000

    implemented types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import "./App.css";
 
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[]; //array of strings
+let role: [number, string]; //tuple
+//object
+type Person = {
+  name: string;
+  age: number;
+};
+let person: Person = {
+  name: "ET",
+  age: 32,
+};
+
 function App() {
   return <div className="App">Hello World</div>;
 }
