dad0c946dca49bcca30d75ba681c4740edbc60cf
SirvanD <sirvansh@gmail.com>
2022-06-30 16:34:32 +1000

    designed form component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./styles.css";
 
-function InputField() {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function InputField({ todo, setTodo }: Props) {
   return (
     <form className="input">
       <input type="input" placeholder="Enter a task" className="input__box" />
