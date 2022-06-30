afec6ea2d985e9548d30a5b6dde3ac14b9c3d989
SirvanD <sirvansh@gmail.com>
2022-06-30 15:51:41 +1000

    implemented types

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,14 @@
 import React from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
-let name: string;
-//union
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[]; //array of strings
-let role: [number, string]; //tuple
-
-age = "thirty five";
-
-let printAge: (name: string) => void; //or never
-
-function printName(name: string) {
-  console.log(name);
-}
-
-printName("Sirvan");
-
-// object;
-// type Person = {
-//   name: string;
-//   age?: number;
-// };
-// let person: Person = {
-//   name: "ET",
-//   age: 32,
-// };
-
-//interface
-
-interface Person {
-  name: string;
-  age?: number;
-}
-
-// let lotsOfPeople: Person[];
-
-function App() {
-  return <div className="App">Hello World</div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/InputField.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function InputField() {
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a task" className="input__box" />
+      <button className="input__submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+}
+
+export default InputField;
